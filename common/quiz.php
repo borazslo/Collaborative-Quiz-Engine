f009b3742fdb47004248ddfdb4b2ad3b6c6844aa
borazslo <eleklaszlosj@gmail.com>
2021-03-12 22:43:59 +0100

    Timing

--- common/quiz.php
+++ common/quiz.php
@@ -18,7 +18,7 @@ class Quiz {
     function __construct($jsonFile) {
         
         $file = $this->folder.$jsonFile;
-        
+                
         if(!file_exists($file)) throw new Exception("Config file '". $file."' does not exists.");
         if(! $settings = json_decode(file_get_contents($file)) ) throw new Exception("Config file '". $file."' is not valid Json.");
         foreach($settings as $key => $val) {
@@ -32,21 +32,59 @@ class Quiz {
             foreach($settings->questions as $key => $question) {
                 $question->id = $key + 1;
                 $question->quiz_id = $this->id;
-                
+                                
+                //Cheking folders
                 if(isset($question->folder)) {                                        
                     $question->folder = $this->folder.$question->folder;
                     if(!is_dir($question->folder)) throw new Exception('There is no folder called '.$question->folder);
                 }
-                
-                //Miért futtatjuk sokszor? Bár ráérünk
                 if(in_array($question->type,['multi','puzzle']) AND !is_dir(TMP_FOLDER.$this->id)) throw new Exception('There is a need for folder called '.TMP_FOLDER.$this->id);
                 
+                //Load question
                 $className = "question".ucfirst($question->type);
-                //if(!class_exists($className)) $className = 'Question';
                 $this->questions[] = new $className($question);
             }
+            $this->loadQuestionsStartEnd();
+            $this->deleteInactiveQuestions();
+            
         }
         
         
+    }    
+    
+    function loadQuestionsStartEnd() {
+        $start = strtotime(isset($this->timing->start) ? $this->timing->start : "today midnight" ); 
+        $frequency = isset($this->timing->frequency) ? ( strtotime($this->timing->frequency) - time() ) : 0 ; 
+        $duration = isset($this->timing->duration) ? ( strtotime($this->timing->duration) - time() ) : 31556952 ;
+                
+        foreach($this->questions as &$question) {
+            if(!isset($last_start))
+                $question->startTime = $start;          
+            elseif(!isset($question->relativeStart))
+                $question->startTime = $last_start + $frequency;
+            else                
+                $question->startTime = strtotime($question->relativeStart,$last_start);
+            
+            if(!isset($question->duration))
+                $question->endTime = $question->startTime + $duration;
+            else
+                $question->endTime = strtotime($question->duration,$question->startTime);
+            
+            $last_start = $question->startTime;
+        }                
+    }
+    
+    /**
+     * Delete inactive Questions based on startTime - endTime
+     */
+    function deleteInactiveQuestions() {
+        global $user; 
+
+        $now = time();
+        foreach($this->questions as $key => $question) {            
+            if( ( $question->startTime < $now OR $question->endTime > $now ) AND !isset($user->isAdmin)) {
+                unset($this->questions[$key]);            
+            }
+        }
     }
 }
