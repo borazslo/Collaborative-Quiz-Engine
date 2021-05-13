c5716717fec812abda261b1bd524b8fb2d9d87ed
borazslo <eleklaszlosj@gmail.com>
2021-05-13 20:50:42 +0200

    questionCompletion hardcore

--- common/questionCompletion.php
+++ common/questionCompletion.php
@@ -10,11 +10,22 @@ class questionCompletion extends Question {
             $c = $this->pseudoRandom(0, count($this->text) - 1 , $this->setUnique() );                        
             $words = explode(' ',$this->text[$c]);
             
-            $k = $this->pseudoRandom(0,count($words)-1, $user->id );
+            $k = $this->pseudoRandom(0,count($words)-1, $user->id );            
             
-            preg_match('/\p{L}+/ui',$words[$k],$match);
-            $this->answer = $match[0];
-            $words[$k] = str_replace($match[0], '_____', $words[$k]);
+            if(isset($this->hardcore)) { 
+                foreach($words as $key => $word) {
+                    preg_match('/\p{L}+/ui',$word,$match);
+                    
+                    if($k == $key) {
+                        $char = '?';
+                        $this->answer = $match[0];
+                    } else $char = '.';
+                    
+                    $words[$key] = str_replace($match[0], str_repeat($char, mb_strlen($match[0])), $word);
+                }            
+            } else {
+                $words[$k] = str_replace($match[0], '_____', $words[$k]);
+            }
             $this->question .= "<br/><blockquote class='blockquote'>".implode(" ",$words)."</blockquote>";                                               
         }
 }
